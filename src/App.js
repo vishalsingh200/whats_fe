8e4c9922a67f0d39fb20ea1ad506ada149ac7ec2
Vishal Kumar Singh <94474346+vishalsingh200@users.noreply.github.com>
2024-02-07 00:53:11 +0530

    setup react router dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+//pages
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
 function App() {
   return (
-    <div className="dark"></div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element = {<Home />} />
+          <Route path="/login" element = {<Login />} />
+          <Route path="/register" element = {<Register />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
