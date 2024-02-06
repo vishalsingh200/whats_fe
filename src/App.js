624d5a8363b01160817ab11a9f19d2034efff46e
Vishal Kumar Singh <94474346+vishalsingh200@users.noreply.github.com>
2024-02-07 01:41:47 +0530

    redux tookit store and dispatch actions

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Register from "./pages/register";
+import { useDispatch } from "react-redux";
+import { logout } from "./features/userSlice";
 function App() {
+
+  const dispatch = useDispatch();
+
   return (
     <div>
       <Router>
+        <button 
+        onClick= { () => {
+          dispatch(logout());
+        }}>logout</button>
         <Routes>
           <Route path="/" element = {<Home />} />
           <Route path="/login" element = {<Login />} />
